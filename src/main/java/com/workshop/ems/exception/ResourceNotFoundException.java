8ead34681ed1f230b0d8c5bcbb63464f9d16cd84
Giga Amiridze <giga.amiridze@azry.com>
2024-03-25 13:09:09 +0400

    Added resource not found exception

---
+++ src/main/java/com/workshop/ems/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.workshop.ems.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
