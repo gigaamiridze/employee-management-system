56f3b0786e174847a1b351f17f160b5210e4e774
Giga Amiridze <giga.amiridze@azry.com>
2024-03-25 14:00:57 +0400

    Added employee controller

---
+++ src/main/java/com/workshop/ems/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.workshop.ems.controller;
+
+import com.workshop.ems.dto.EmployeeDto;
+import com.workshop.ems.model.EmployeeResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/ems/employee")
+public interface EmployeeController {
+
+    @PostMapping("/create")
+    ResponseEntity<EmployeeResponse> createEmployee(@RequestBody EmployeeDto employeeDto);
+}
---
+++ src/main/java/com/workshop/ems/controller/impl/EmployeeControllerImpl.java
@@ -0,0 +1,25 @@
+package com.workshop.ems.controller.impl;
+
+import com.workshop.ems.controller.EmployeeController;
+import com.workshop.ems.dto.EmployeeDto;
+import com.workshop.ems.model.EmployeeResponse;
+import com.workshop.ems.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeControllerImpl implements EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeControllerImpl(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @Override
+    public ResponseEntity<EmployeeResponse> createEmployee(EmployeeDto employeeDto) {
+        return employeeService.createEmployee(employeeDto);
+    }
+}
